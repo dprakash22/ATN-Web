06eaf77b41b1489694905b09c223f948d2abe905
dprakash22 <deepakprakash.s2022cse@sece.ac.in>
2024-04-14 15:36:09 +0530

    Started

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+import First from './components/First'
+
+const App=()=>{
+  return (
+    <First/>
+  )
+}
+export default App
---
+++ src/components/First.jsx
@@ -0,0 +1,19 @@
+import '../assets/first.jpg'
+import '../assets/ATN logo.png'
+import '../assets/firstcss.css';
+
+const First=()=>{
+    return (
+        <>
+        <div className='container'>
+        <img className='water'  width={'1535px'} height={'700px'} src="src/assets/first.jpg" alt="pic"></img>
+        <img className='logo' src='src/assets/ATN logo.png' alt='pic'></img>
+        <div className='circle'></div>
+        <div className='circle1'></div>
+        <div className='circle2'></div>
+        </div>
+        
+        </>
+    )
+}
+export default First
