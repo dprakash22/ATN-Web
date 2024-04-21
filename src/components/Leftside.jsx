22c5f9844341a3efccbcdc4a24146941f50f2e1d
dprakash22 <deepakprakash.s2022cse@sece.ac.in>
2024-04-21 22:37:26 +0530

    suuma

--- src/components/Leftside.jsx
+++ src/components/Leftside.jsx
@@ -1,6 +1,9 @@
 import { Outlet } from 'react-router-dom'
-import '../assets/images/ATN logo.png'
-import '../assets/images/globe1.png'
+import summa from '../assets/images/ATN logo.png'
+// import deepa from '../assets/images/globe1.png'
+import deepa from "../assets/images/complete1.png"
+import deva from "../assets/images/help.jpg"
+import dd from "../assets/images/pic-1.png"
 import '../assets/leftside.css'
 import Dashboard from '../pages/Dashboard'
 import Product from '../pages/Product'
@@ -11,15 +14,15 @@ const Leftside=()=>{
         <>
         <div className='requestpage'>
         <div className="left">
-            <img className='R-logo' width={'120px'} src='src/assets/images/ATN logo.png' alt="pic"></img>
+            <img className='R-logo' width={'120px'} src={summa} alt="pic"></img>
             <div className='l-title'>
                 <div className='titles'>
-                    <img width={'45px'} style={{marginLeft:'3px',marginTop:'6px'}} src='src/assets/images/pic-1.png' alt='pic'></img>
+                    <img width={'45px'} style={{marginLeft:'3px',marginTop:'6px'}} src={dd} alt='pic'></img>
                     <h4 style={{ marginTop: '10px', marginLeft:'10px',marginBottom:'5px' }}>Incoming Request   <code>&gt;</code> </h4>
                 </div>
 
                 <div className='titles'>
-                    <img width={'40px'} style={{marginLeft:'3px',marginTop:'6px'}} src='src/assets/images/complete1.png' alt='pic'></img>
+                    <img width={'40px'} style={{marginLeft:'3px',marginTop:'6px'}} src={deepa} alt='pic'></img>
                     <h4 style={{ marginTop: '14px', marginLeft:'10px',marginBottom:'5px' }}>Completed Request  <code>&gt;</code></h4>
                 </div>
 
@@ -29,7 +32,7 @@ const Leftside=()=>{
                 </div>
 
                 <div className='titles'>
-                    <img width={'40px'} style={{marginLeft:'3px',marginTop:'6px'}} src='src/assets/images/help.jpg' alt='pic'></img>
+                    <img width={'40px'} style={{marginLeft:'3px',marginTop:'6px'}} src={deva} alt='pic'></img>
                     {/* <MessageSquareMore /> */}
                     <h4 style={{ marginTop: '10px', marginLeft:'14px',marginBottom:'5px' }}>Help  <code>&gt;</code></h4>
                 </div>
